29ab922e437cfb947349504fe1675c3c242961c5
Xaxxis <zaki.ibrahim@midtrans.com>
2019-09-18 10:49:36 +0700

    change project module structure

---
+++ src/main/java/com/midtrans/httpclient/CoreApi.java
@@ -0,0 +1,53 @@
+package com.midtrans.httpclient;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.Map;
+
+/**
+ * HttpClient for midtrans CoreAPI
+ */
+public interface CoreApi {
+
+    @GET("token")
+    Call<ResponseBody> cardToken(@QueryMap Map<String, String> params);
+
+    @GET("card/register")
+    Call<ResponseBody> registerCard(@QueryMap Map<String, String> params);
+
+    @GET("point_inquiry/{tokenId}")
+    Call<ResponseBody> cardPointInquiry(@Path("tokenId") String tokenId);
+
+    @POST("charge")
+    Call<ResponseBody> chargeTransaction(@Body Map<String, Object> body);
+
+    @GET("{orderId}/status")
+    Call<ResponseBody> checkTransaction(@Path("orderId") String orderId);
+
+    @POST("{orderId}/approve")
+    Call<ResponseBody> approveTransaction(@Path("orderId") String orderId);
+
+    @POST("{orderId}/deny")
+    Call<ResponseBody> denyTransaction(@Path("orderId") String orderId);
+
+    @POST("{orderId}/cancel")
+    Call<ResponseBody> cancelTransaction(@Path("orderId") String orderId);
+
+    @POST("{orderId}/expire")
+    Call<ResponseBody> expireTransaction(@Path("orderId") String orderId);
+
+    @POST("{orderId}/refund")
+    Call<ResponseBody> refundTransaction(@Path("orderId") String orderId, @Body Map<String, String> body);
+
+    @POST("{orderId}/refund/online/direct")
+    Call<ResponseBody> directRefundTransaction(@Path("orderId") String orderId, @Body Map<String, String> body);
+
+    @POST("capture")
+    Call<ResponseBody> captureTransaction(@Body Map<String, String> body);
+
+    @GET("{orderId}/status/b2b")
+    Call<ResponseBody> getStatusB2B(@Path("orderId") String orderId);
+
+}
---
+++ src/main/java/com/midtrans/httpclient/SnapApi.java
@@ -0,0 +1,18 @@
+package com.midtrans.httpclient;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+import java.util.Map;
+
+/**
+ * HttpClient for midtrans SnapAPI
+ */
+public interface SnapApi {
+
+    @POST("transactions")
+    Call<ResponseBody> createTransactions(@Body Map<String, Object> objectMap);
+
+}
