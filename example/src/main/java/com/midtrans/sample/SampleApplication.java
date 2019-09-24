24e4e437b25c1e9b83dada189694a8c5d29ba7fc
Xaxxis <zaki.ibrahim@midtrans.com>
2019-09-24 09:32:42 +0700

    initial commit

--- example/src/main/java/com/midtrans/sample/SampleApplication.java
+++ example/src/main/java/com/midtrans/sample/SampleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
