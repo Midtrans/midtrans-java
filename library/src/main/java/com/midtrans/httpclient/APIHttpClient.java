fe21e0132e87d96971f70c0d8e5041f089dfee93
Zaki Ibrahim <xaxxis.id@gmail.com>
2020-05-13 13:53:42 +0700

    Update HttpClient
    - Added connection pool configuration
    - change Http headers setup, added some headers params
    - change conditional logic LogingInterceptor
    - added new method to get library version

--- library/src/main/java/com/midtrans/httpclient/APIHttpClient.java
+++ library/src/main/java/com/midtrans/httpclient/APIHttpClient.java
@@ -4,12 +4,15 @@ import com.midtrans.Config;
 import com.midtrans.proxy.ProxyConfig;
 import okhttp3.*;
 import okhttp3.Authenticator;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -21,7 +24,6 @@ public class APIHttpClient {
 
     private Config config;
     private OkHttpClient defaultHttpClient;
-
 
     /**
      * APIHttpClient constructor
@@ -40,11 +42,19 @@ public class APIHttpClient {
      */
     private OkHttpClient configHttpClient() {
         OkHttpClient httpClient = new OkHttpClient();
+
+        ConnectionPool connectionPool = new ConnectionPool(
+                config.getMaxConnectionPool(),
+                config.getKeepAliveDuration(),
+                TimeUnit.SECONDS);
+
         if (config.getProxyConfig() == null) {
             return httpClient.newBuilder()
+                    .addInterceptor(loggingInterceptor())
                     .connectTimeout(config.getConnectionTimeout(), TimeUnit.SECONDS)
                     .readTimeout(config.getReadTimeout(), TimeUnit.SECONDS)
                     .writeTimeout(config.getWriteTimeout(), TimeUnit.SECONDS)
+                    .connectionPool(connectionPool)
                     .addInterceptor(headers())
                     .build();
         }
@@ -59,6 +69,7 @@ public class APIHttpClient {
                 .connectTimeout(config.getConnectionTimeout(), TimeUnit.SECONDS)
                 .readTimeout(config.getReadTimeout(), TimeUnit.SECONDS)
                 .writeTimeout(config.getWriteTimeout(), TimeUnit.SECONDS)
+                .connectionPool(connectionPool)
                 .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyConfig.getHost(), proxyConfig.getPort())))
                 .proxyAuthenticator(proxyAuthenticator)
                 .addInterceptor(headers())
@@ -78,17 +89,30 @@ public class APIHttpClient {
                 }
                 return chain.proceed(chain.request());
             }
+            Map<String, String> headersMap = new HashMap<>();
+            headersMap.put("Accept", "application/json");
+            headersMap.put("Content-Type", "application/json");
+            headersMap.put("Authorization", encodeServerKey());
+            headersMap.put("User-Agent", "Midtrans-Java-Library-"+getLibraryVersion());
+            if (!config.getIrisIdempotencyKey().isEmpty()) {
+                headersMap.put("X-Idempotency-Key", config.getIrisIdempotencyKey());
+            }
+            if (!config.getPaymentIdempotencyKey().isEmpty()) {
+                headersMap.put("Idempotency-Key", config.getPaymentIdempotencyKey());
+            }
+            if (!config.getPaymentAppendNotification().isEmpty()) {
+                headersMap.put("X-Append-Notification", config.getPaymentAppendNotification());
+            }
+            if (!config.getPaymentOverrideNotification().isEmpty()) {
+                headersMap.put("X-Override-Notification", config.getPaymentOverrideNotification());
+            }
 
-            Request header = chain.request().newBuilder()
-                    .addHeader("Accept", "application/json")
-                    .addHeader("Authorization", this.encodeServerKey())
-                    .addHeader("Content-Type", "application/json")
-                    .addHeader("User-Agent", "Midtrans-Java-Library")
-                    .build();
-                return chain.proceed(header);
+            Headers headers = Headers.of(headersMap);
+            Request request = chain.request().newBuilder().headers(headers).build();
+
+            return chain.proceed(request);
         };
     }
-
 
     /**
      * get client Retrofit configuration
@@ -103,9 +127,28 @@ public class APIHttpClient {
                 .build();
     }
 
+    /**
+     * OkHttpClient Logging Interceptor Configuration. Auto disable when production mode
+     *
+     * @return HttpLoggingInterceptor
+     */
+    private HttpLoggingInterceptor loggingInterceptor() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        if (config.isEnabledLog()) {
+            return logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        } else {
+            return logging.setLevel(HttpLoggingInterceptor.Level.NONE);
+        }
+    }
+
     private String encodeServerKey(){
         return "Basic " + Base64.getEncoder().encodeToString((config.getSERVER_KEY() + ":").getBytes(StandardCharsets.UTF_8));
     }
+
+    private String getLibraryVersion() throws IOException {
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("/version.properties");
+        Properties properties = new Properties();
+        properties.load(resourceAsStream);
+        return (properties.getProperty("version") == null) ? "unable to reach" : properties.getProperty("version");
+    }
 }
-
-
