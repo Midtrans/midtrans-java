9b9fe737ccf57245a7db36ee0d06ec31319cb4b2
Zaki Ibrahim <xaxxis.id@gmail.com>
2020-05-13 14:00:38 +0700

    Update constant for testing purpose

--- library/src/test/java/com/midtrans/java/mockupdata/Constant.java
+++ library/src/test/java/com/midtrans/java/mockupdata/Constant.java
@@ -1,13 +1,17 @@
 package com.midtrans.java.mockupdata;
 
 public class Constant {
-    public static final String serverKey = "SB-Mid-server-Wh2cEDd4H661g4lrcig8sQMf";
-    public static final String clientKey = "SB-Mid-client-N7N5b2n_ZUQOOVba";
+    public static final String serverKey = "SB-Mid-server-TOq1a2AVuiyhhOjvfs3U_KeO";
+    public static final String clientKey = "SB-Mid-client-nKsqvar5cn60u2Lv";
     public static boolean isProduction = false;
 
-    public static final String COREAPI_PRODUCTION_BASE_URL = "https://api.midtrans.com/v2/";
-    public static final String COREAPI_SANDBOX_BASE_URL = "https://api.sandbox.midtrans.com/v2/";
-    public static final String SNAP_PRODUCTION_BASE_URL = "https://app.midtrans.com/snap/v1/";
-    public static final String SNAP_SANDBOX_BASE_URL = "https://app.sandbox.midtrans.com/snap/v1/";
+    public static final String COREAPI_PRODUCTION_BASE_URL = "https://api.midtrans.com/";
+    public static final String COREAPI_SANDBOX_BASE_URL = "https://api.sandbox.midtrans.com/";
+    public static final String SNAP_PRODUCTION_BASE_URL = "https://app.midtrans.com/";
+    public static final String SNAP_SANDBOX_BASE_URL = "https://app.sandbox.midtrans.com/";
 
+    public static final String creatorKey = "IRIS-330198f0-e49d-493f-baae-585cfded355d";
+    public static final String approverKey = "IRIS-1595c12b-6814-4e5a-bbbb-9bc18193f47b";
+    public static final String IRIS_SANDBOX_BASE_URL = "https://app.sandbox.midtrans.com/iris/";
+    public static final String IRIS_PRODUCTION_BASE_URL = "https://app.midtrans.com/iris/";
 }
