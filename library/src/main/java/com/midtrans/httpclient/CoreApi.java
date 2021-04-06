f90d0603903692d43ea539d14476b7ab0440fc2d
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-04-06 17:59:23 +0700

    Change api class from retrofit using okhttp

--- library/src/main/java/com/midtrans/httpclient/CoreApi.java
+++ library/src/main/java/com/midtrans/httpclient/CoreApi.java
@@ -1,59 +1,65 @@
 package com.midtrans.httpclient;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.http.*;
+import com.midtrans.Config;
+import com.midtrans.httpclient.error.MidtransError;
+import org.json.JSONObject;
 
 import java.util.Map;
 
-/**
- * HttpClient for midtrans CoreAPI
- */
-public interface CoreApi {
 
-    String API_VERSION = "v2/";
+public class CoreApi {
 
-    @GET(API_VERSION + "token")
-    Call<ResponseBody> cardToken(@QueryMap Map<String, String> params);
+    public static final String API_VERSION = "v2";
 
-    @GET(API_VERSION + "card/register")
-    Call<ResponseBody> registerCard(@QueryMap Map<String, String> params);
+    public static JSONObject cardToken(Map<String, String> param, Config configOptions) throws MidtransError {
+        String url = Config.getGlobalConfig().getCoreApiURL() + API_VERSION +
+                "/token?client_key=" + param.get("client_key") +
+                "&card_number=" + param.get("card_number") +
+                "&card_exp_month=" + param.get("card_exp_month") +
+                "&card_exp_year=" + param.get("card_exp_year") +
+                "&card_cvv=" + param.get("card_cvv");
+        return new JSONObject((String) APIHttpClient.request(APIHttpClient.GET, url, configOptions, null));
+    }
 
-    @GET(API_VERSION + "point_inquiry/{tokenId}")
-    Call<ResponseBody> cardPointInquiry(@Path("tokenId") String tokenId);
+    public static JSONObject cardToken(Map<String, String> param) throws MidtransError {
+        return cardToken(param, Config.getGlobalConfig());
+    }
 
-    @Headers({"Content-Type: application/json"})
-    @POST(API_VERSION + "charge")
-    Call<ResponseBody> chargeTransaction(@Body Map<String, Object> body);
+    public static JSONObject registerCard(Map<String, String> param, Config configOptions) throws MidtransError {
+        String url = Config.getGlobalConfig().getCoreApiURL() + API_VERSION +
+                "/card/register?client_key=" + param.get("client_key") +
+                "&card_number=" + param.get("card_number") +
+                "&card_exp_month=" + param.get("card_exp_month") +
+                "&card_exp_year=" + param.get("card_exp_year") +
+                "&card_cvv=" + param.get("card_cvv");
+        return new JSONObject((String) APIHttpClient.request(APIHttpClient.GET, url, configOptions, null));
+    }
 
-    @GET(API_VERSION + "{orderId}/status")
-    Call<ResponseBody> checkTransaction(@Path("orderId") String orderId);
+    public static JSONObject registerCard(Map<String, String> param) throws MidtransError {
+        return registerCard(param, Config.getGlobalConfig());
+    }
 
-    @POST(API_VERSION + "{orderId}/approve")
-    Call<ResponseBody> approveTransaction(@Path("orderId") String orderId);
+    public static JSONObject cardPointInquiry(String tokenId, Config configOptions) throws MidtransError {
+        return new JSONObject((String) APIHttpClient.request(APIHttpClient.GET, configOptions.getCoreApiURL() + API_VERSION + "/point_inquiry/" + tokenId, configOptions, null));
+    }
 
-    @POST(API_VERSION + "{orderId}/deny")
-    Call<ResponseBody> denyTransaction(@Path("orderId") String orderId);
+    public static JSONObject cardPointInquiry(String tokenId) throws MidtransError {
+        return cardPointInquiry(tokenId, Config.getGlobalConfig());
+    }
 
-    @POST(API_VERSION + "{orderId}/cancel")
-    Call<ResponseBody> cancelTransaction(@Path("orderId") String orderId);
+    public static JSONObject chargeTransaction(Map<String, Object> requestBody, Config configOptions) throws MidtransError {
+        return new JSONObject((String) APIHttpClient.request(APIHttpClient.POST, configOptions.getCoreApiURL() + API_VERSION + "/charge", configOptions, requestBody));
+    }
 
-    @POST(API_VERSION + "{orderId}/expire")
-    Call<ResponseBody> expireTransaction(@Path("orderId") String orderId);
+    public static JSONObject chargeTransaction(Map<String, Object> requestBody) throws MidtransError {
+        return chargeTransaction(requestBody, Config.getGlobalConfig());
+    }
 
-    @POST(API_VERSION + "{orderId}/refund")
-    Call<ResponseBody> refundTransaction(@Path("orderId") String orderId, @Body Map<String, String> body);
+    public static JSONObject getBin(String binNumber, Config configOptions) throws MidtransError {
+        return new JSONObject((String) APIHttpClient.request(APIHttpClient.GET, configOptions.getCoreApiURL() + "v1" + "/bins/" + binNumber, configOptions, null));
+    }
 
-    @POST(API_VERSION + "{orderId}/refund/online/direct")
-    Call<ResponseBody> directRefundTransaction(@Path("orderId") String orderId, @Body Map<String, String> body);
-
-    @POST(API_VERSION + "capture")
-    Call<ResponseBody> captureTransaction(@Body Map<String, String> body);
-
-    @GET(API_VERSION + "{orderId}/status/b2b")
-    Call<ResponseBody> getStatusB2B(@Path("orderId") String orderId);
-
-    @GET("v1/bins/{bin}")
-    Call<ResponseBody> getBIN(@Path("bin") String binNumber);
-
+    public static JSONObject getBin(String binNumber) throws MidtransError {
+        return getBin(binNumber, Config.getGlobalConfig());
+    }
 }
