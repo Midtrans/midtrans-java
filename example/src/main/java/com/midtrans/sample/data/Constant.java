ea5ac12d41428398d50508a57ce16ab97b9dbd72
Zaki Ibrahim <xaxxis.id@gmail.com>
2021-04-06 17:54:38 +0700

    Update example: add new constant and DataMockup order-id prefix

---
+++ example/src/main/java/com/midtrans/sample/data/Constant.java
@@ -0,0 +1,11 @@
+package com.midtrans.sample.data;
+
+public final class Constant {
+
+    public static final String sandboxServerKey = "SB-Mid-server-TOq1a2AVuiyhhOjvfs3U_KeO";
+    public static final String sandboxClientKey = "SB-Mid-client-nKsqvar5cn60u2Lv";
+    public static final String secondServerKey = "SB-Mid-server-9Nm5c-HJE65AjLNtTX-bRjqm";
+    public static final String secondClientKey = "SB-Mid-client-GtbzK39rvs5El-bC";
+
+    public static boolean isProduction = false;
+}
